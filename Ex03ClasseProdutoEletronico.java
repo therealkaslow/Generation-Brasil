7b40c8235a59a14184fe9850c52efe68c3596371
Lucas Reis <88865748+therealkaslow@users.noreply.github.com>
2021-09-09 00:00:42 -0300

    Exercicios do dia 08 de Setembro

---
+++ Ex03ClasseProdutoEletronico.java
@@ -0,0 +1,20 @@
+package Exercicios;
+
+public class Ex03ClasseProdutoEletronico {
+	private String nomeProduto = "";
+	private double preco = 0;
+	
+	public String getNomeProduto() {
+		return nomeProduto;
+	}
+	public void setNomeProduto(String nomeProduto) {
+		this.nomeProduto = nomeProduto;
+	}
+	public double getPreco() {
+		return preco;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+}
